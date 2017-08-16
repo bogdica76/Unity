9e51a1a9c0b824ed6660985ffa89477714f1e299
Ionescu Bogdan <s.ionescu@attosoft.ro>
2017-08-16 09:44:11 +0300

    baker house

---
+++ WorldOfEnglish/Assets/_Scripts/Player/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	GameObject aPlayer;
+
+	// Use this for initialization
+	void Start () {
+		aPlayer = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = aPlayer.transform.position;
+		transform.rotation = aPlayer.transform.rotation;
+	}
+}
