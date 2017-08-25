d642cb11bb0ff0b8a78e9629b4870894b9cfaa38
Ionescu Bogdan <s.ionescu@attosoft.ro>
2017-08-25 15:50:36 +0300

    first interactions

---
+++ WorldOfEnglish/Assets/_Scripts/Managers/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+	public static DialogueManager Instance { get; set;}
+
+	public GameObject dialoguePanel;
+
+	public string npcName;
+
+	public List<string> dialogueLines = new List<string>();
+
+	Button okButton;
+	Text dialogueText, nameText;
+	int dialogueIndex;
+	// Use this for initialization
+	void Awake () {
+		okButton = dialoguePanel.transform.Find ("Ok").GetComponent<Button> ();
+		dialogueText = dialoguePanel.transform.Find ("Text").GetComponent<Text> ();
+		nameText = dialoguePanel.transform.Find ("Name").GetChild (0).GetComponent<Text> ();
+
+		dialoguePanel.SetActive (false);
+
+		okButton.onClick.AddListener (delegate {
+			ContinueDialogue ();
+		});
+
+		if (Instance != null && Instance != this) {
+			Destroy (gameObject);
+		} else {
+			Instance = this;
+		}
+	}
+	
+	public void AddNewDialogue(string[] lines, string npcName){
+		dialogueIndex = 0;
+		dialogueLines = new List<string>(lines.Length);
+		foreach (string line in lines) {
+			dialogueLines.Add (line);
+		}
+		this.npcName = npcName;
+		CreateDialogue ();
+	}
+
+	public void CreateDialogue(){
+		dialogueText.text = dialogueLines [dialogueIndex];
+		nameText.text = npcName;
+		dialoguePanel.SetActive (true);
+	}
+
+	public void ContinueDialogue(){
+		if (dialogueIndex < dialogueLines.Count - 1) {
+			dialogueIndex++;
+			dialogueText.text = dialogueLines [dialogueIndex];
+		} else {
+			dialoguePanel.SetActive (false);
+		}
+	}
+}
--- WorldOfEnglish/Assets/_Scripts/Player/WorldInteraction.cs
+++ WorldOfEnglish/Assets/_Scripts/Player/WorldInteraction.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class WorldInteraction : MonoBehaviour {
 	NavMeshAgent playerAgent;
@@ -10,10 +11,37 @@ public class WorldInteraction : MonoBehaviour {
 		playerAgent = GetComponent<NavMeshAgent> ();
 	}
 
+	private bool IsPointerOverUIObject() {
+		PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+		eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+		List<RaycastResult> results = new List<RaycastResult>();
+		EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+		return results.Count > 0;
+	}
+
 	void Update () {
-		if (Input.GetMouseButtonDown (0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+	/*	#if UNITY_ANDROID
+		Debug.Log("android");
+		if (
+			Input.GetMouseButtonDown (0) && 
+			!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)
+		)
+		{
+			GetInteraction ();
+			return;
+		}
+		#endif*/
+
+
+		if (
+			Input.GetMouseButtonDown (0) && 
+			!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() &&
+			!IsPointerOverUIObject()
+			)
+		{
 			GetInteraction ();
 		}
+
 	}
 
 	void GetInteraction(){
@@ -23,7 +51,9 @@ public class WorldInteraction : MonoBehaviour {
 			GameObject interactedObject = interactionInfo.collider.gameObject;
 			if (interactedObject.tag == "interactable") {
 				Debug.Log ("Interacted");
+				interactedObject.GetComponent<Interactable> ().MoveToInteraction (playerAgent);
 			} else {
+				playerAgent.stoppingDistance = 0f;
 				playerAgent.destination = interactionInfo.point;
 			}
 		}
