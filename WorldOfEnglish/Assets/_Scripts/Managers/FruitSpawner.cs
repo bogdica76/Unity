0e6e8996cb59237127ec24886b8bbda1741e15d5
Ionescu Bogdan <s.ionescu@attosoft.ro>
2017-10-30 10:44:38 +0200

    modificari prostesti

---
+++ WorldOfEnglish/Assets/_Scripts/Managers/FruitSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour {
+
+	private ArrayList Fruits;
+	private ArrayList FruitsSpawned;
+
+	// Use this for initialization
+	void Start () {
+//		Fruits.Add("apple", "pear", "banana");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SpawnFruit(){
+
+	}
+}
