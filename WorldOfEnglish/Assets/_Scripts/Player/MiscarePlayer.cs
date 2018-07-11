8c7283f18125f749a133a0eeea30b0758ea93fe3
Dorian <email@email.com>
2018-07-11 18:05:02 +0300

    harta joc

---
+++ WorldOfEnglish/Assets/_Scripts/Player/MiscarePlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiscarePlayer : MonoBehaviour {
+
+	public float speed = 15.0f;
+	public float rotationSpeed = 150.0f;
+
+	void Start(){
+
+	}
+
+void Update()
+    {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+    }
+}
